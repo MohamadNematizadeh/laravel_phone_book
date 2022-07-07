2a652665e595fd1278122b74e18898ff14c72663
mohamad.nematizadeh <89206040+mnbvcxz09156006138@users.noreply.github.com>
2022-07-07 23:00:56 +0430

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MessageController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[MessageController::class, "index"])->name('message.index');
+Route::post('/send-form',[MessageController::class, "send"]);
+Route::delete('/destroy/{message}' , [MessageController::class, "destroy"])->name('message.destroy');
